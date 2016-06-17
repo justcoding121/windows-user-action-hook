105cf52ff2f2da47b04e0d93bc1f8d7c1d929937
justcoding121 <jehonathant@gmail.com>
2016-06-17 01:28:03 -0400

    update msbuild

--- .build/default.ps1
+++ .build/default.ps1
@@ -27,7 +27,7 @@ Import-Module "$Here\Common" -DisableNameChecking
 
 $NuGet = Join-Path $SolutionRoot ".nuget\nuget.exe"
 
-$MSBuild ="${env:ProgramFiles(x86)}\MSBuild\12.0\Bin\msbuild.exe"
+$MSBuild ="${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\msbuild.exe"
 
 FormatTaskName (("-"*25) + "[{0}]" + ("-"*25))
 
@@ -52,7 +52,7 @@ Task Restore-Packages  {
 }
 
 Task Install-MSBuild {
-    if(!(Test-Path "${env:ProgramFiles(x86)}\MSBuild\12.0\Bin\msbuild.exe")) 
+    if(!(Test-Path "${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\msbuild.exe")) 
 	{ 
 		cinst microsoft-build-tools -y
 	}
