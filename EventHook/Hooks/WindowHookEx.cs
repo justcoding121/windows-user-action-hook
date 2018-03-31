721cea99abe3b46838cd08ecdba95a49cbf5ad89
Victor Milovanov <lost@losttech.software>
2018-03-31 11:20:32 -0700

    Longer message for hook disposal error

--- EventHook/Hooks/WindowHookEx.cs
+++ EventHook/Hooks/WindowHookEx.cs
@@ -110,7 +110,9 @@
 
         void ReleaseUnmanagedResources(bool disposing) {
             lock (this.hooks) {
-                Debug.Assert(this.hooks.Count == 0 || disposing, "Somebody forgot to dispose the hook");
+                Debug.Assert(this.hooks.Count == 0 || disposing, "Somebody forgot to dispose the hook. "
+                    + "That will cause heap corruption, because the hook handler "
+                    + "will be disposed before hooking is disabled by the code below.");
                 foreach (IntPtr hook in this.hooks.Values) {
                     if (!UnhookWinEvent(hook)) {
                         var error = new Win32Exception();
