ddd68d54622218d090aa85668954ad553306b200
justcoding121 <jehonathant@gmail.com>
2018-04-29 18:30:31 -0400

    add docs

--- EventHook/EventHookFactory.cs
+++ EventHook/EventHookFactory.cs
@@ -3,6 +3,12 @@ using EventHook.Helpers;
 
 namespace EventHook
 {
+    /// <summary>
+    /// A factory class core to the management of various watchers 
+    /// that all shares the same synchronization objects.
+    /// Use this class to get instances of differant watchers.
+    /// This factory instance should be disposed only after all watchers it have been unsubscribed.
+    /// </summary>
     public class EventHookFactory : IDisposable
     {
         private readonly SyncFactory syncFactory = new SyncFactory();
@@ -12,26 +18,46 @@ namespace EventHook
             syncFactory.Dispose();
         }
 
+        /// <summary>
+        /// Get an instance of application watcher.
+        /// </summary>
+        /// <returns></returns>
         public ApplicationWatcher GetApplicationWatcher()
         {
             return new ApplicationWatcher(syncFactory);
         }
 
+        /// <summary>
+        /// Get an instance of keystroke watcher.
+        /// </summary>
+        /// <returns></returns>
         public KeyboardWatcher GetKeyboardWatcher()
         {
             return new KeyboardWatcher(syncFactory);
         }
 
+        /// <summary>
+        /// Get an instance of mouse watcher.
+        /// </summary>
+        /// <returns></returns>
         public MouseWatcher GetMouseWatcher()
         {
             return new MouseWatcher(syncFactory);
         }
 
+        /// <summary>
+        /// Get an instance of clipboard watcher.
+        /// </summary>
+        /// <returns></returns>
         public ClipboardWatcher GetClipboardWatcher()
         {
             return new ClipboardWatcher(syncFactory);
         }
 
+        /// <summary>
+        /// Get an instance of print watcher.
+        /// </summary>
+        /// <returns></returns>
         public PrintWatcher GetPrintWatcher()
         {
             return new PrintWatcher(syncFactory);
