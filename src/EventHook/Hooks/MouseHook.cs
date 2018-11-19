33394b685575514fc1eac402ba87226ea21c6e86
Jan Berdel <jan.berdel@glueckkanja.com>
2018-11-19 11:29:56 +0100

    Expose MouseData and define WM_XBUTTON

--- src/EventHook/Hooks/MouseHook.cs
+++ src/EventHook/Hooks/MouseHook.cs
@@ -11,6 +11,7 @@ namespace EventHook.Hooks
     {
         internal MouseMessages Message { get; set; }
         internal Point Point { get; set; }
+        internal uint MouseData { get; set; }
     }
 
     /// <summary>
@@ -25,7 +26,9 @@ namespace EventHook.Hooks
         WM_RBUTTONDOWN = 0x0204,
         WM_RBUTTONUP = 0x0205,
         WM_WHEELBUTTONDOWN = 0x207,
-        WM_WHEELBUTTONUP = 0x208
+        WM_WHEELBUTTONUP = 0x208,
+        WM_XBUTTONDOWN = 0x020B,
+        WM_XBUTTONUP = 0x020C
     }
 
     /// <summary>
@@ -94,7 +97,7 @@ namespace EventHook.Hooks
 
             hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
 
-            MouseAction(null, new RawMouseEventArgs { Message = (MouseMessages)wParam, Point = hookStruct.pt });
+            MouseAction(null, new RawMouseEventArgs { Message = (MouseMessages)wParam, Point = hookStruct.pt, MouseData = hookStruct.mouseData });
 
             return CallNextHookEx(_hookId, nCode, wParam, lParam);
         }
